5229b1998fa720cd77d4cfcf754591737fdf612b
Jason <j_schubb53@yahoo.com>
2021-04-06 16:48:41 -0400

    first commit

---
+++ NumberAnalyzer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace NumberAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int userChoice;
+            int oddEven;
+
+            do
+            {
+                Console.WriteLine("What is your name? ");
+                name = Console.ReadLine();
+
+                while (true)
+                {
+
+                    Console.WriteLine("Please choose a number between 1 - 100.");
+                    userChoice = int.Parse(Console.ReadLine());
+
+                    oddEven = userChoice % 2;
+
+                    if (oddEven == 1 && userChoice <= 100)
+                    {
+                        Console.WriteLine($"{ name } {userChoice} " + "Odd.");
+                    }
+                    else if (oddEven == 0 && userChoice >= 2 && userChoice <= 25)
+                    {
+                        Console.WriteLine($"{ name } " + "your number is Even and less than 25.");
+                    }
+                    else if (oddEven == 0 && userChoice >= 26 && userChoice <= 60)
+                    {
+                        Console.WriteLine($"{ name } " + "your number is Even.");
+                    }
+                    else if (oddEven == 0 && userChoice >= 60 && userChoice <= 100)
+                    {
+                        Console.WriteLine($"{ name } {userChoice} " + "Even.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is not a valid input");
+                        continue;
+                    }
+
+                    Console.WriteLine("Do you want to try again yes or no");
+                    while (true)
+                    {
+                        string result = Console.ReadLine();
+                        if (result == "yes")
+                        {
+                            break;
+                        }
+                        else if (result == "no")
+                        {
+                            Console.WriteLine("Thank you for participating");
+                            Environment.Exit(0);
+                        }
+                        else
+                        {
+                            Console.WriteLine("That was not a yes or no");
+                        }
+                    }
+                }
+            } while (true);
+            
+            
+        }
+    }
+}
